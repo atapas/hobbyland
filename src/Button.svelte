4d31bf15b6b8a9e489b86a7eb1e79faaf93d0430
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-07-20 16:23:19 +0530

    feat: Added the slider for weight

--- src/Button.svelte
+++ src/Button.svelte
@@ -1,11 +1,11 @@
 <style>
     button{
-          font : larger;
-          padding: 0.15rem 0.5rem;
           background-color: #1b1a1a;
           border: 1px solid  aliceblue ;
           cursor: pointer;
           color: white;
+          border-radius: 0.25rem;
+          text-transform: uppercase;
       }
   
   </style>
